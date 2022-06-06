51a7d7eedb76d9f9a60ed04426dc2bd293396a05
Pablo Rica <pabloricaaldeguer@gmail.com>
2022-06-06 02:00:36 +0200

    fixing CodeSniffer bugs

--- app/public/wp-content/themes/instant-understrap-child/functions.php
+++ app/public/wp-content/themes/instant-understrap-child/functions.php
@@ -1,62 +1,88 @@
 <?php
 /**
  * Understrap Child Theme functions and definitions
+ * php version 7.4.1
  *
- * @package UnderstrapChild
+ * @category   Category
+ * @package    Understrap
+ * @subpackage UnderstrapChild
+ * @author     Pablo Rica <pablo@codigo.co.uk>
+ * @license    MIT 
+ * @version    GIT: @1.0.0@
+ * @link       link
+ * @since      UnderstrapChild 1.0
  */
-
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
-
+defined('ABSPATH') || exit;
 
 
 /**
  * Removes the parent themes stylesheet and scripts from inc/enqueue.php
+ *
+ * @return void
  */
-function understrap_remove_scripts() {
-	wp_dequeue_style( 'understrap-styles' );
-	wp_deregister_style( 'understrap-styles' );
+function Understrap_Remove_scripts() 
+{
+    wp_dequeue_style('understrap-styles');
+    wp_deregister_style('understrap-styles');
 
-	wp_dequeue_script( 'understrap-scripts' );
-	wp_deregister_script( 'understrap-scripts' );
+    wp_dequeue_script('understrap-scripts');
+    wp_deregister_script('understrap-scripts');
 }
-add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
-
+add_action('wp_enqueue_scripts', 'Understrap_Remove_scripts', 20);
 
 
 /**
  * Enqueue our stylesheet and javascript file
+ *
+ * @return void
  */
-function theme_enqueue_styles() {
+function Theme_Enqueue_styles()
+{
+    // Get the theme data.
+    $the_theme = wp_get_theme();
 
-	// Get the theme data.
-	$the_theme = wp_get_theme();
+    $suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
+    // Grab asset urls.
+    $theme_styles  = "/css/child-theme{$suffix}.css";
+    $theme_scripts = "/js/child-theme{$suffix}.js";
 
-	$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
-	// Grab asset urls.
-	$theme_styles  = "/css/child-theme{$suffix}.css";
-	$theme_scripts = "/js/child-theme{$suffix}.js";
-
-	wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . $theme_styles, array(), $the_theme->get( 'Version' ) );
-	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . $theme_scripts, array(), $the_theme->get( 'Version' ), true );
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    wp_enqueue_style(
+        'child-understrap-styles', 
+        get_stylesheet_directory_uri() . $theme_styles, 
+        array(), 
+        $the_theme->get('Version')
+    );
+    wp_enqueue_script('jquery');
+    wp_enqueue_script(
+        'child-understrap-scripts', 
+        get_stylesheet_directory_uri() . $theme_scripts, 
+        array(), 
+        $the_theme->get('Version'), 
+        true
+    );
+    if (is_singular() 
+        && comments_open() 
+        && get_option('thread_comments')
+    ) {
+        wp_enqueue_script('comment-reply');
+    }
 }
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-
-
+add_action('wp_enqueue_scripts', 'Theme_Enqueue_styles');
 
 /**
  * Load the child theme's text domain
+ *
+ * @return void
  */
-function add_child_theme_textdomain() {
-	load_child_theme_textdomain( 'instant-understrap-child', get_stylesheet_directory() . '/languages' );
+function Add_Child_Theme_textdomain()
+{
+    load_child_theme_textdomain(
+        'instant-understrap-child', 
+        get_stylesheet_directory() . '/languages'
+    );
 }
-add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
-
-
+add_action('after_setup_theme', 'Add_Child_Theme_textdomain');
 
 /**
  * Overrides the theme_mod to default to Bootstrap 5
@@ -65,25 +91,35 @@ add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
  * can be duplicated to override other theme settings.
  *
  * @param string $current_mod The current value of the theme_mod.
+ * 
  * @return string
  */
-function understrap_default_bootstrap_version( $current_mod ) {
-	return 'bootstrap5';
+function Understrap_Default_Bootstrap_version( $current_mod ) 
+{
+    return 'bootstrap5';
 }
-add_filter( 'theme_mod_understrap_bootstrap_version', 'understrap_default_bootstrap_version', 20 );
-
-
+add_filter(
+    'theme_mod_understrap_bootstrap_version', 
+    'Understrap_Default_Bootstrap_version', 
+    20
+);
 
 /**
  * Loads javascript for showing customizer warning dialog.
+ *
+ * @return void
  */
-function understrap_child_customize_controls_js() {
-	wp_enqueue_script(
-		'understrap_child_customizer',
-		get_stylesheet_directory_uri() . '/js/customizer-controls.js',
-		array( 'customize-preview' ),
-		'20130508',
-		true
-	);
+function Understrap_Child_Customize_Controls_js() 
+{
+    wp_enqueue_script(
+        'understrap_child_customizer',
+        get_stylesheet_directory_uri() . '/js/customizer-controls.js',
+        array( 'customize-preview' ),
+        '20130508',
+        true
+    );
 }
-add_action( 'customize_controls_enqueue_scripts', 'understrap_child_customize_controls_js' );
+add_action(
+    'customize_controls_enqueue_scripts', 
+    'Understrap_Child_Customize_Controls_js'
+);
